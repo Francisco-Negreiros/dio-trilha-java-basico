60bfae4ca4ac7b7f5eb23dc1ec2f9cef0d13bf73
francisco-negreiros <negreiros.fv@gmail.com>
2025-09-24 10:00:17 -0300

    aula sobre operadores em java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,35 @@
+public class Operadores {
+    public static void main(String[] args) {
+        /*String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+
+        int numero = 5;
+
+        numero = numero + 5;
+        
+        System.out.println(numero);
+
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);*/
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = "";
+        //String resultado = a == b ? "verdadeiro" : "falso";
+        /*if (a == b) {
+            System.out.println("verdadeiro");
+        } else {
+            System.out.println("falso");        
+            
+        }
+        System.out.println(resultado);
+    }*/
+        resultado = a != b ? "verdadeiro" : "falso";
+        System.out.println(resultado);
+    }
+    
+}
