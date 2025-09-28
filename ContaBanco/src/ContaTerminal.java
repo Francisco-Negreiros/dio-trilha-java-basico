3bb6df866f8502d4868d16bcab373ce1ab8bf807
francisco-negreiros <negreiros.fv@gmail.com>
2025-09-27 21:47:07 -0300

    criada a interação com o cliente

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,8 +4,18 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        
 
+        System.out.println("Por favor, digite o número da conta:");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Por favor, digite o número da agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o nome do cliente:");
+        String nomeCliente = scanner.next();    
+
+        System.out.println("Por favor, digite o saldo da conta:");
+        double saldo = scanner.nextDouble();    
 
         scanner.close();
     }
