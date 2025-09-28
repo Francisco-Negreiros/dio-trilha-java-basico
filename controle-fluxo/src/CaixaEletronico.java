50a4d168529736d431aa0b8f9d45a9ea5bbf7b88
francisco-negreiros <negreiros.fv@gmail.com>
2025-09-28 17:53:14 -0300

    criado ResultadoEscolar

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,12 +1,14 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 40.0;
 
         if(valorSolicitado < saldo){
             saldo -= valorSolicitado;
 
-            System.out.println(saldo);
+            System.out.println("Valor Solicitado: " +saldo);
+        }else{
+            System.out.println("Saldo insulficiente!");
         }
         
     }
